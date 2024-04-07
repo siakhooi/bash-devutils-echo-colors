efacbb50cccf37833e4de1eea0e9d624dfe34453
Siak Hooi <73884114+siakhooi@users.noreply.github.com>
2024-04-07 09:59:32 +0000

    add cat.blue, cat.cyan, cat.green, cat.grey, cat.red, cat.magenta, cat.yellow

--- scripts/git-commit-and-push.sh
+++ scripts/git-commit-and-push.sh
@@ -3,12 +3,14 @@ set -e
 
 . ./release.env
 
-if [[ -n $ISSUE_TO_CLOSE ]]; then
-  GIT_MESSAGE="$RELEASE_TITLE, Close $ISSUE_TO_CLOSE"
-else
+if [ $ISSUE_TO_CLOSE == ""]; then
   GIT_MESSAGE="$RELEASE_TITLE"
+else
+  GIT_MESSAGE="$RELEASE_TITLE, Close $ISSUE_TO_CLOSE"
 fi
 
+echo $GIT_MESSAGE
+exit 
 set -x
 
 git commit -m "$GIT_MESSAGE"
